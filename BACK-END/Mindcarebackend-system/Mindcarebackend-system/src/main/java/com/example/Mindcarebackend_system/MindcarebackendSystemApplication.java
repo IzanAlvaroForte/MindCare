5d87fec41f4f96c43395b8c1b34f4015e294f986
Izanalvaroforte <yullarjhonmora@gmail.com>
2026-05-06 12:06:14 +0800

    backend

---
+++ BACK-END/Mindcarebackend-system/Mindcarebackend-system/src/main/java/com/example/Mindcarebackend_system/MindcarebackendSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mindcarebackend_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindcarebackendSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindcarebackendSystemApplication.class, args);
+	}
+
+}
---
+++ BACK-END/Mindcarebackend-system/Mindcarebackend-system/src/test/java/com/example/Mindcarebackend_system/MindcarebackendSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mindcarebackend_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindcarebackendSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
